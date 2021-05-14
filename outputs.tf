92f6ed91d8fbd0e9ba478c8bf5c5150dfc0c2e3b
Laurent Piroelle <laurent.piroelle@fr.clara.net>
2021-05-14 16:32:23 +0200

    AZ-50: Name output

--- outputs.tf
+++ outputs.tf
@@ -3,6 +3,11 @@ output "scale_set_id" {
   value       = azurerm_linux_virtual_machine_scale_set.linux-vmss.0.id
 }
 
+output "scale_set_name" {
+  description = "Scale Set Name"
+  value       = azurerm_linux_virtual_machine_scale_set.linux-vmss.0.name
+}
+
 output "system_assigned_identity" {
   description = "Identity block with principal ID"
   value       = azurerm_linux_virtual_machine_scale_set.linux-vmss.0.identity
