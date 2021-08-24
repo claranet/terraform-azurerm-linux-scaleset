9c5970fe5d5549e1f52c90c119dbc1a0b73f353b
Rémi Beaufils <remi.beaufils@fr.clara.net>
2021-08-24 15:11:20 +0200

    AZ-50: Fix automatic OS upgrade policy condition

--- r-vmss.tf
+++ r-vmss.tf
@@ -87,7 +87,7 @@ resource "azurerm_linux_virtual_machine_scale_set" "linux-vmss" {
 
 
   dynamic "automatic_os_upgrade_policy" {
-    for_each = var.upgrade_mode != "Manual" ? ["fake"] : []
+    for_each = var.upgrade_mode == "Automatic" ? ["fake"] : []
     content {
       disable_automatic_rollback  = var.disable_automatic_rollback
       enable_automatic_os_upgrade = var.automatic_os_upgrade
